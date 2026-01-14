097decc8ce030068600bd80af46b5e073eee1f91
Arun Kumar Patnana <arunkumar.vtalent@gmail.com>
2026-01-14 11:28:46 +0530

    Home and Create page code

--- mypro/myapp/myappurls.py
+++ mypro/myapp/myappurls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('home/', views.home, name="home"),
     path('create/', views.create, name="create"),
-    path('edit/<int:EmpId>/', views.edit, name="edit"),
+    path('edit/<int:EmpId>/', views.edit, name="edit"), #Query parameter
     path('delete/<int:EmpId>/', views.delete, name="delete"),
     path('<path:pagename>/', views.pagenotfound, name="pagenotfound")
 ]
--- mypro/myapp/views.py
+++ mypro/myapp/views.py
@@ -1,11 +1,42 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
+from .models import Department, Employee
 
 # Create your views here.
+def Validations(data):
+    pass
+
+
 def home(request):
-    return render(request, 'home.html')
+    empList = Employee.objects.all()
+    return render(request, 'home.html', { 'empList' : empList })
 
 def create(request):
-    return render(request, 'create.html')
+    if request.method == "GET":
+        deptList = Department.objects.all()
+        return render(request, 'create.html', { 'deptList' : deptList })
+    else:
+        if request.POST.get("action") == "Register":
+            # try:                
+                #Insert the data into the database table : By using model class object
+                Validations(request.POST)
+
+                dept = Department.objects.get(DeptNo = (request.POST.get('DeptNo')))            
+                Employee.objects.create(
+                    EmpId = request.POST.get('EmpId'),
+                    Ename = request.POST.get('Ename'),
+                    Password = request.POST.get('Password'),
+                    Gender = request.POST.get('Gender'),
+                    Dob = request.POST.get('Dob'),
+                    Phone = request.POST.get('Phone'),
+                    Email = request.POST.get('Email'),
+                    Salary = request.POST.get('Salary'),
+                    Address = request.POST.get('Address'),
+                    DeptNo = dept
+                )
+            # except Exception:
+            #     return HttpResponse("Something wrong happend...!")
+            
+        return redirect('home')
 
 def edit(request, EmpId):
     return render(request, 'edit.html')
@@ -15,3 +46,6 @@ def delete(request, EmpId):
 
 def pagenotfound(request):
     return render(request, 'pagenotfound.html')
+
+# def httpExceptionHandle(request, exception):
+#     return render(request, <Create one Html>, {'exception' : exception})
